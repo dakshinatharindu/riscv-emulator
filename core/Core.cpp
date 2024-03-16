fa308ae84fc530cce2d67e597d977ba5a3ef687c
dakshinatharindu <tharinduokd@gmail.com>
2024-03-17 02:59:22 +0530

    add LOAD and STORE instructions

--- core/Core.cpp
+++ core/Core.cpp
@@ -3,11 +3,13 @@
 //
 
 // Opcodes
-#define LUI     0b0110111
-#define AUIPC   0b0010111
-#define JAL     0b1101111
-#define JALR    0b1100111
-#define BRANCH  0b1100011
+#define LUI 0b0110111
+#define AUIPC 0b0010111
+#define JAL 0b1101111
+#define JALR 0b1100111
+#define BRANCH 0b1100011
+#define LOAD 0b0000011
+#define STORE 0b0100011
 
 #include "Core.h"
 #include <iostream>
@@ -18,6 +20,7 @@ Core::Core() {
     for (int i = 0; i < 32; i++) {
         this->reg[i] = 0;
     }
+    this->mem = new uint8_t[1024];
 }
 
 // Destructor
@@ -29,70 +32,138 @@ void Core::run(int instrt) {
         case LUI: {
             uint32_t imm = instrt & 0xFFFFF000;
             uint32_t rd = (instrt >> 7) & 0x1F;
-            if (rd) this->reg[rd] = imm;
+            if (rd)
+                this->reg[rd] = imm;
             this->pc += 4;
             break;
         }
         case AUIPC: {
             uint32_t imm = instrt & 0xFFFFF000;
             uint32_t rd = (instrt >> 7) & 0x1F;
-            if (rd) this->reg[rd] = this->pc + imm;
+            if (rd)
+                this->reg[rd] = this->pc + imm;
             this->pc += 4;
             break;
         }
         case JAL: {
-            uint32_t imm = (instrt & 0x80000000) >> 11 | (instrt & 0x7FE00000) >> 20 | (instrt & 0x100000) >> 9 |
-                           (instrt & 0xFF000);
-            if (imm & 0x80000) imm |= 0xFFF00000;
+            uint32_t imm = (instrt & 0x80000000) >> 11 | (instrt & 0x7FE00000) >> 20 |
+                           (instrt & 0x100000) >> 9 | (instrt & 0xFF000);
+            if (imm & 0x80000)
+                imm |= 0xFFF00000;
             uint32_t rd = (instrt >> 7) & 0x1F;
-            if (rd) this->reg[rd] = this->pc + 4;
+            if (rd)
+                this->reg[rd] = this->pc + 4;
             this->pc += imm;
             break;
         }
         case JALR: {
             uint32_t imm = (instrt & 0xFFF00000) >> 20;
-            if (imm & 0x800) imm |= 0xFFFFF000;
+            if (imm & 0x800)
+                imm |= 0xFFFFF000;
             uint32_t rs1 = (instrt >> 15) & 0x1F;
             uint32_t rd = (instrt >> 7) & 0x1F;
-            if (rd) this->reg[rd] = this->pc + 4;
+            if (rd)
+                this->reg[rd] = this->pc + 4;
             this->pc = (this->reg[rs1] + imm) & 0xFFFFFFFE;
             break;
         }
         case BRANCH: {
-            uint32_t imm = (instrt & 0x80000000) >> 19 | (instrt & 0x7E000000) >> 20 | (instrt & 0xF00) >> 7 |
-                           (instrt & 0x80) << 4;
-            if (imm & 0x800) imm |= 0xFFFFF000;
+            uint32_t imm = (instrt & 0x80000000) >> 19 | (instrt & 0x7E000000) >> 20 |
+                           (instrt & 0xF00) >> 7 | (instrt & 0x80) << 4;
+            if (imm & 0x800)
+                imm |= 0xFFFFF000;
             int32_t rs1_val = this->reg[(instrt >> 15) & 0x1F];
             int32_t rs2_val = this->reg[(instrt >> 20) & 0x1F];
             switch ((instrt >> 12) & 0x7) {
                 case 0b000: // BEQ
-                    if (rs1_val == rs2_val) this->pc += imm;
-                    else this->pc += 4;
+                    if (rs1_val == rs2_val)
+                        this->pc += imm;
+                    else
+                        this->pc += 4;
                     break;
                 case 0b001: // BNE
-                    if (rs1_val != rs2_val) this->pc += imm;
-                    else this->pc += 4;
+                    if (rs1_val != rs2_val)
+                        this->pc += imm;
+                    else
+                        this->pc += 4;
                     break;
                 case 0b100: // BLT
-                    if (rs1_val < rs2_val) this->pc += imm;
-                    else this->pc += 4;
+                    if (rs1_val < rs2_val)
+                        this->pc += imm;
+                    else
+                        this->pc += 4;
                     break;
                 case 0b101: // BGE
-                    if (rs1_val >= rs2_val) this->pc += imm;
-                    else this->pc += 4;
+                    if (rs1_val >= rs2_val)
+                        this->pc += imm;
+                    else
+                        this->pc += 4;
                     break;
                 case 0b110: // BLTU
-                    if ((uint32_t) rs1_val < (uint32_t) rs2_val) this->pc += imm;
-                    else this->pc += 4;
+                    if ((uint32_t) rs1_val < (uint32_t) rs2_val)
+                        this->pc += imm;
+                    else
+                        this->pc += 4;
                     break;
                 case 0b111: // BGEU
-                    if ((uint32_t) rs1_val >= (uint32_t) rs2_val) this->pc += imm;
-                    else this->pc += 4;
+                    if ((uint32_t) rs1_val >= (uint32_t) rs2_val)
+                        this->pc += imm;
+                    else
+                        this->pc += 4;
                     break;
                 default:
                     std::cout << "Unknown branch opcode" << std::endl;
             }
             break;
+        }
+        case LOAD: {
+            uint32_t imm = instrt >> 20;
+            if (imm & 0x800) imm |= 0xFFFFF000;
+            uint32_t rs1 = (instrt >> 15) & 0x1F;
+            uint32_t rd = (instrt >> 7) & 0x1F;
+            uint32_t addr = this->reg[rs1] + imm;
+            uint32_t load_val = 0;
+            switch ((instrt >> 12) & 0x7) {
+                case 0b000: // LB
+                    load_val = *((int8_t *) (this->mem + addr));
+                    break;
+                case 0b001: // LH
+                    load_val = *((int16_t *) (this->mem + addr));
+                    break;
+                case 0b010: // LW
+                    load_val = *((uint32_t *) (this->mem + addr));
+                    break;
+                case 0b100: // LBU
+                    load_val = *((uint8_t *) (this->mem + addr));
+                    break;
+                case 0b101: // LHU
+                    load_val = *((uint16_t *) (this->mem + addr));
+                    break;
+                default:
+                    std::cout << "Unknown load opcode" << std::endl;
+            }
+            if (rd) this->reg[rd] = load_val;
+        }
+        case STORE: {
+            uint32_t imm = ((instrt >> 25) & 0x7F) << 5 | (instrt >> 7) & 0x1F;
+            if (imm & 0x800) imm |= 0xFFFFF000;
+            uint32_t rs1 = (instrt >> 15) & 0x1F;
+            uint32_t rs2 = (instrt >> 20) & 0x1F;
+            uint32_t addr = this->reg[rs1] + imm;
+            uint32_t store_val = this->reg[rs2];
+            switch ((instrt >> 12) & 0x7) {
+                case 0b000: // SB
+                    *((uint8_t *) (this->mem + addr)) = store_val;
+                    break;
+                case 0b001: // SH
+                    *((uint16_t *) (this->mem + addr)) = store_val;
+                    break;
+                case 0b010: // SW
+                    *((uint32_t *) (this->mem + addr)) = store_val;
+                    break;
+                default:
+                    std::cout << "Unknown store opcode" << std::endl;
+            }
         }
         default:
             std::cout << "Unknown opcode" << std::endl;
@@ -106,4 +177,3 @@ void Core::print_state() {
         std::cout << "x" << i << ": " << (int32_t) this->reg[i] << std::endl;
     }
 }
-
--- core/main.cpp
+++ core/main.cpp
@@ -7,8 +7,10 @@
 
 int main() {
     Core core;
-    core.run(0xebbff36f);
-    core.run(0xebbff36f);
+    core.run(0x0ea00267);
+//    core.run(0x0ea00267);
+    core.run(0x00402023);
+    core.run(0x00005283);
 
     core.print_state();
     return 0;
