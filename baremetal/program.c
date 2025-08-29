6d08b18b9bfe6ae4a98fdfa1bd5c49a0666c6197
dakshinatharindu <dakshinatharindu99@gmail.com>
2025-08-29 22:49:12 +0000

    change compiler and add init_jump section

--- baremetal/program.c
+++ baremetal/program.c
@@ -1,30 +1,12 @@
-extern char uartbuffer[256];
-extern char uarthead;
-extern char uarttail;
-
-void print(const char *str) {
-    char c;
-	uarttail = 0;
-    while (c = *(str++)) {
-		if (uarttail == 255) {
-			asm volatile("ecall");
-        }
-		uartbuffer[uarttail] = c;
-    	uarttail++;
-	}
-    asm volatile("ecall");
+int multiply(int a, int b) {
+  int result;
+  result = a * b;
+  return result;
 }
 
 int main() {
-    int a, b, c;
-    a = 1;
-    b = 2;
-    c = a + b;
-    print("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ornare leo sit amet ante molestie lacinia. Aliquam venenatis ut velit in cursus. Ut non porttitor quam. Maecenas vel feugiat nulla, in condimentum est. Fusce pharetra quam at metus aeadsf adsfa sdfk\n");
-}
-
-int _start() {
-    main();
-    asm volatile("ebreak");
-    return 0;
+  int a = 6;
+  int b = 7;
+  int product = multiply(a, b);
+  return 0;
 }
